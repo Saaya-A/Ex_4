9524cd576be1bca55edf68831bb40c909db7def3
saydi <saydi@ENGINEER>
2022-06-07 18:13:03 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int[] a = new int[10];
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = random.Next(0, 20);
+            Console.WriteLine(a[i]);
+        }
+        for (int i = a.Length - 1; i >= 0; i--)
+        {
+            int j = random.Next(i);
+            int d = a[i];
+            a[i] = a[j];
+            a[j] = d;
+        }
+        Console.WriteLine();
+        Console.WriteLine("Преобразованный массив: ");
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+    }
+}
